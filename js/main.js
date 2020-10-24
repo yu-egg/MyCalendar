c0b061c173e35693ea208151e8fe6c8d0571c7d1
yu-egg <69953750+yu-egg@users.noreply.github.com>
2020-10-24 17:39:19 +0900

    月末の日付を取得し、配列に今月分の日付を入れていきます。  getCalendarBody() push()

--- js/main.js
+++ js/main.js
@@ -3,4 +3,16 @@
 {
   const year = 2020;
   const month = 4; //5月
+
+  function getCalenderBody() {
+    const dates = []; // date: 日付, day:曜日
+    const lastDate = new Date(year, month + 1, 0).getDate();
+
+    for (let i = 1; i <= lastDate; i++){
+      dates.push(i);
+    }
+    console.log(dates);
+  }
+
+  getCalenderBody();
 }
